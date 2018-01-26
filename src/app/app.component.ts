46c85b5947cb61b7968f0c32ee4646221400c7ef
Karim Dallas-Newell <karimnewell@Karims-iMac.local>
2018-01-26 15:03:41 -0500

    emit game control number to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentNumber: number;
+
+  onUpdateNumber(number: number) {
+    this.currentNumber = number;
+  }
 }
---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+// import { setInterval } from 'timers';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+  number = 0;
+  @Output() emitNumber = new EventEmitter<number>();
+  ref;
+  disableStart = false;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  incrementNumber() {
+    this.number++;
+    this.emitNumber.emit(this.number);
+    console.log(this.number);
+  }
+
+  startGame() {
+    this.ref = setInterval(() => {
+      this.incrementNumber();
+    }, 1000);
+    this.disableStart = true;
+  }
+
+  endGame() {
+    clearInterval(this.ref);
+    this.disableStart = false;
+  }
+
+}
