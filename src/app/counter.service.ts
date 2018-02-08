41dae9c0429f1e3a06fe0432f4302c60859d2999
Karim Dallas-Newell <karim@devslow.tech>
2018-02-08 11:57:01 -0500

    added properties to counterService

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CounterService {
-
+  count = 0;
   constructor() { }
 
+  increment() {
+    console.log(++this.count);
+  }
 }
